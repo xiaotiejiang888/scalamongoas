187236ed54dc423f5eadf54c1eea1974eb2904d1
maoyu.zhang <maoyu.zhang@tendcloud.com>
2017-07-13 10:16:53 +0800

    remove useless code

--- src/main/scala/com/mongo/spark/MongoFactory.scala
+++ src/main/scala/com/mongo/spark/MongoFactory.scala
@@ -1,16 +1,13 @@
-import com.mongodb.casbah.{MongoCollection, MongoConnection, MongoURI}
+import com.mongodb.casbah.{MongoConnection, MongoURI}
 
 object MongoFactory {
   
-  private val SERVER     = "172.23.6.82"
+  private val SERVER     = "172.23.5.158"
   private val PORT       = 27017
   private val DATABASE   = "mpush"
-  private val COLLECTION = "stocks"
 
   @throws(classOf[Exception])
   def getConnection: MongoConnection = return MongoConnection(MongoURI("mongodb://mpush:talkingdata@172.23.6.82/mpush"))
-  @throws(classOf[Exception])
-  def getCollection(conn: MongoConnection): MongoCollection = return conn(DATABASE)(COLLECTION)
   @throws(classOf[Exception])
   def closeConnection(conn: MongoConnection) { conn.close }
 
