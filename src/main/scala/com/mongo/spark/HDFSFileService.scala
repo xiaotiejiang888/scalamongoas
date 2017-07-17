850c3d478e4f650676b29d9589092d442b5ad41a
maoyu.zhang <maoyu.zhang@tendcloud.com>
2017-07-17 15:39:34 +0800

    add main

--- src/main/scala/com/mongo/spark/HDFSFileService.scala
+++ src/main/scala/com/mongo/spark/HDFSFileService.scala
@@ -1,12 +1,15 @@
 package com.mongo.spark
 
-import java.io.{BufferedInputStream, File, FileInputStream, InputStream}
+import java.io.{FileSystem => _, _}
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Date}
 
+import com.mongodb.spark.config._
+import com.mongodb.spark.sql._
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
 
 object HDFSFileService {
   private val conf = new Configuration()
@@ -70,14 +73,6 @@ object HDFSFileService {
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder()
-      .appName("MongoSparkConnectorTour").master("spark://172.23.5.113:7077")
-      .getOrCreate()
-    val df = spark.sqlContext.read.json("hdfs://wifianalytics/user/zmy/td_2017-07-06.txt");
-    df.show();
-  }
   def getOneDayStart_time():Long={
     val now = new Date()
     val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
@@ -94,5 +89,49 @@ object HDFSFileService {
     val a = cal.getTime.getTime
     var str = a+""
     str.toLong
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc = getSparkContext(args)
+    val sqlContext = SQLContext.getOrCreate(sc)
+    val dfApp = sqlContext.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://zmy3:zmy3@172.23.5.158/mpush.app?readPreference=primary", "partitioner" -> "MongoSplitVectorPartitioner")))
+    val dfTd = sqlContext.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://zmy3:zmy3@172.23.5.158/mpush.td?readPreference=primary", "partitioner" -> "MongoSplitVectorPartitioner")))
+    val dfTdApp = sqlContext.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://zmy3:zmy3@172.23.5.158/mpush.td?readPreference=primary", "partitioner" -> "MongoSplitVectorPartitioner"))).where("source='app'")
+    val dfTdAppWithOutId = dfTdApp.drop("_id")
+    val dfTdGame = sqlContext.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://zmy3:zmy3@172.23.5.158/mpush.td?readPreference=primary", "partitioner" -> "MongoSplitVectorPartitioner"))).where("source='game'")
+    val dfTdGameWithOutId = dfTdGame.drop("_id")
+    val dfPush = sqlContext.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://zmy3:zmy3@172.23.5.158/mpush.push?readPreference=primary", "partitioner" -> "MongoSplitVectorPartitioner")))
+    val dfActivity = sqlContext.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://zmy3:zmy3@172.23.5.158/mpush.activity?readPreference=primary", "partitioner" -> "MongoSplitVectorPartitioner")))
+    val sprkAsconf = new SparkConf().setMaster("spark://172.23.5.113:7077").setAppName("Aerospike Tests for Spark Dataset").set("aerospike.seedhost", "172.23.5.158").set("aerospike.port",  "3000").set("aerospike.namespace", "push")
+    val spark = SparkSession.builder().config(sprkAsconf).master("spark://172.23.5.113:7077").appName("Aerospike Tests").getOrCreate()
+    val dfDv = spark.read.format("com.aerospike.spark.sql").option("aerospike.set", "dv").load
+    var appApp = dfApp.join(dfTdAppWithOutId,dfApp("_id")===dfTdAppWithOutId("app"))
+    var appGame = dfApp.join(dfTdGameWithOutId,dfApp("_id")===dfTdGameWithOutId("app"))
+    dfApp.registerTempTable("dfApp")
+    dfTd.registerTempTable("dfTd")
+    var appMpush = spark.sqlContext.sql("select * from dfApp  where _id not in (select app from dfTd)")
+    var count = appApp.join(dfPush,appApp("_id")===dfPush("app")).select(dfPush("_id"),dfPush("ct")).where("ct> 1499616000000 and ct< 1499702400000").count()
+    val fileName = "统计结果_" + getDt + ".txt"
+    val writer = new PrintWriter(new File(fileName))
+    writer.write("app每日贡献push数:"+count)
+    writer.close()
+  }
+  def getDt = {
+    val now: Date = new Date()
+    val dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val dt = dateFormat.format(now)
+    dt
+  }
+  def getSparkContext(args: Array[String]): SparkContext = {
+    val uri: String = args.headOption.getOrElse("mongodb://zmy3:zmy3@172.23.5.158/mpush.app?readPreference=primary")
+    val conf = new SparkConf()
+      .setMaster("spark://172.23.5.113:7077")
+      .setAppName("MongoSparkConnectorTour")
+      .set("spark.app.id", "MongoSparkConnectorTour")
+      .set("spark.mongodb.input.uri", uri)
+      .set("spark.mongodb.output.uri", uri)
+
+    val sc = new SparkContext(conf)
+    sc
   }
 }
